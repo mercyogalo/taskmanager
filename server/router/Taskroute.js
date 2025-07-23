f81f8d3902ffa6205391d5c5298068d0e8cb3afc
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-07-23 11:09:13 +0300

    Added the task router

--- server/router/Taskroute.js
+++ server/router/Taskroute.js
@@ -2,3 +2,54 @@ const express=require('express');
 const Task=require('../models/Task');
 const router=express.Router();
 
+
+router.post('/', async(req, res)=>{
+     try {
+        const task=new Task(req.body);
+        await task.save();
+        res.status(201).json(task);
+        
+     } catch (error) {
+        res.status(400).json({error: error.message});
+     }
+})
+
+router.put('/task/:id', async(req, res)=>{
+
+    try {
+         const updateTask=await Task.findByIdAndUpdate(
+        req.body,
+        req.params.id,
+        { new: true}
+    )
+
+    res.json(updateTask);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+   
+
+})
+
+router.get('/tasks', async(req,res)=>{
+   try {
+    const tasks=await Task.find();
+    res.json(tasks);
+   } catch (error) {
+    res.status(500).json({error:error.mesage})
+   }
+
+})
+
+router.delete('/task/:id', async(req, res)=>{
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.json({message:'Task deleted successfully'});
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+})
+
+module.exports(router);
--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const dotenv=require('dotenv');
+const taskRouter=require('./router/Taskroute');
 
 const PORT=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/api', taskRouter);
 
 mongoose.connect(process.env.MONGO_URI,
   {})
